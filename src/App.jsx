05d358da3944455d454ef9eabd2f01069ff4e8f6
SergiGom <lol10100020@gmail.com>
2026-03-11 11:17:06 -0500

    Proyecto Cats Completo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Gallery from './pages/Gallery'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <Gallery>
+
+    </Gallery>
+
+
+    </>
+  )
+}
+
+export default App
+
